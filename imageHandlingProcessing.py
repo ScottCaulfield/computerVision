3f8db4e893cd670c0aace3dcfddda9075cd144a6
ScottCaulfield <scott.caulfield99@gmail.com>
2022-07-23 21:01:34 +0100

    Using matplotlib

--- imageHandlingProcessing.py
+++ imageHandlingProcessing.py
@@ -7,18 +7,18 @@ originalImage = Image.open('Images/ClashOfCosmos.png')
 # greyImage = Image.open('Images/ClashOfCosmos.png').convert('L')
 # greyImage.show()
 
-""" Creates a thumbnail of the image preserving aspect ratios with 128x128 max resolution """
+# Creates a thumbnail of the image preserving aspect ratios with 128x128 max resolution
 # originalImage.thumbnail((128, 28))
 
-""" Defines a region of the image (left, upper, right, lower) and crops to that region """
+# Defines a region of the image (left, upper, right, lower) and crops to that region
 # box = (100, 100, 400, 400)
 # region = originalImage.crop(box)
 
-""" Rotates the cropped region by 180 degrees and places it back on the image """
+# Rotates the cropped region by 180 degrees and places it back on the image
 # region = region.transpose(Image.ROTATE_180)
 # originalImage.paste(region, box)
 
-""" Resizes the image and rotates it """
+# Resizes the image and rotates it
 # originalImage = originalImage.resize((1280, 1280))
 # originalImage = originalImage.rotate(45)
 
---
+++ matplotlibExamples.py
@@ -0,0 +1,51 @@
+from PIL import Image
+from pylab import *
+
+# Read image to array
+im = array(Image.open('Images/ClashOfCosmos.png').convert('L'))
+
+""" Plotting points """
+
+# # Plot image
+# imshow(im)
+
+# # Arbitrary points
+# x = [100, 100, 400, 400]
+# y = [200, 500, 200, 500]
+
+# # Plot points with red star markers
+# plot(x, y, 'r*')
+
+# # Line plot connecting two of the points (in this case the first and third one)
+# # w+: means white dashed line with pluses
+# plot(x[::3], y[::3], 'w+:')
+
+# # title("Plotting: 'ClashOfCosmos.png'")
+# axis('off') # Comment as necessary
+
+""" Contour image """
+
+# # Create a new figure with no colours
+# figure()
+# gray()
+
+# # Show contours with origin upper left corner
+# contour(im, origin="image")
+# axis('equal')
+# axis('off')
+
+""" Histogram """
+
+# figure()
+
+# # hist() takes a 1D array as input, so flatten() converts any array to a 1D array with values taken row-wise
+# hist(im.flatten(), 128)
+
+""" Interactive annotation """
+
+imshow(im)
+print("Click 3 points")
+x = ginput(3)
+print("You clicked", x)
+
+show()
